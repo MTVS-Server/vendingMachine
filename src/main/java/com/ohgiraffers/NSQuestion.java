6f55bc1fb86a7bcc683e8928e90d7b7c5ace5522
Son Jung In <nanajungin13@gmail.com>
2023-06-01 17:39:45 +0900

    NFQuestion 커밋(#2)

--- src/main/java/com/ohgiraffers/NSQuestion.java
+++ src/main/java/com/ohgiraffers/NSQuestion.java
@@ -1,4 +1,22 @@
 package com.ohgiraffers;
 
+import java.util.Scanner;
+
 public class NSQuestion {
+    public String question(int num){
+
+        System.out.println("당신의 mbti 중 \"정보수집\"을(를) 검사해보겠습니다.");
+        System.out.println("===============================");
+        System.out.println("1. 백설공주");
+        System.out.println("2. 맛있다");
+
+        if (num == 1) {
+            return "N";
+        } else if(num ==2){
+            return "S";
+        }
+        else {
+            throw new IllegalArgumentException("그 외 숫자를 입력하였습니다.");
+        }
+    }
 }
